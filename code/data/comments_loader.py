f236d6f96e32214f49016bb9eaf2134c14ea88d2
Ayush Baid <ayushrakeshbaid@gmail.com>
2020-03-16 10:16:46 -0400

    Adding data loader

---
+++ code/data/comments_loader.py
@@ -0,0 +1,75 @@
+import functools
+import os
+import operator
+import re
+
+import nltk
+import numpy as np
+import torch
+from nltk.corpus import stopwords
+
+from torch.utils.data import Dataset
+
+
+class CommentsDataset(Dataset):
+    def __format_sentences(self, inp: str) -> str:
+        # format the sentences to remove special characters
+
+        # removing subreddit and user links
+        clean_review = re.sub(r'/?(r|u)/\w+/?', '', inp)
+
+        clean_review = re.sub('[^a-zA-Z]', ' ', clean_review)
+
+        # convert to lower case
+        clean_review = clean_review.lower()
+
+        if self.remove_stopwords:
+            return ' '.join([word for word in clean_review.split() if word not in self.stop_words])
+        else:
+            return clean_review
+
+    def __init__(self, base_path, subreddit, remove_stopwords=False):
+        super().__init__()
+
+        nltk.download('stopwords')
+
+        self.remove_stopwords = remove_stopwords
+        self.stop_words = stopwords.words('english')
+
+        tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
+
+        # load the full numpy array
+        npy_path = os.path.join(base_path, subreddit+'.npy')
+        raw_data = np.load(npy_path)
+
+        # process the taw data into sentences
+
+        self.sentences = functools.reduce(
+            operator.iconcat, [tokenizer.tokenize(inp) for inp in raw_data], [])
+
+        self.sentences = list(map(self.__format_sentences,
+                                  filter(lambda x: x !=
+                                         '[removed]', self.sentences)
+                                  ))
+
+    def __len__(self):
+        return len(self.sentences)
+
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+
+        return self.sentences[idx]
+
+    def get_training_set(self):
+        return self.sentences
+
+
+if __name__ == '__main__':
+    obj = CommentsDataset('../dataset/small',
+                          'the_donald', remove_stopwords=True)
+
+    print(obj.__len__())
+
+    for i in range(10):
+        print(obj.__getitem__(i))

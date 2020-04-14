1e3b3b0a28baff050ec97e795f117a63dcbf642c
Ayush Baid <ayushrakeshbaid@gmail.com>
2020-04-13 20:47:54 -0400

    reformatting ulmfit to make it ready for final training

--- code/data/ulfit_data.py
+++ code/data/ulfit_data.py
@@ -1,73 +1,103 @@
-# from fastai.text import *
 import glob
 import os
 import zipfile
 
+import numpy as np
+import pandas as pd
+import fastai.text as fastai_text
+
+
 class DataProcessing(object):
 
-  def get_filenames(self, fldr_path, filetype):
-    path = str(fldr_path/filetype)
-    return glob.glob(path)
+    def get_filenames(self, fldr_path, filetype):
+        path = str(fldr_path/filetype)
+        return glob.glob(path)
 
-  def unzip_data(self, src_fldr, tgt_fldr):
-    with zipfile.ZipFile(src_fldr, "r") as zip_ref:
-      zip_ref.extractall(tgt_fldr)
+    def unzip_data(self, src_fldr, tgt_fldr):
+        with zipfile.ZipFile(src_fldr, "r") as zip_ref:
+            zip_ref.extractall(tgt_fldr)
 
-  def create_csv(self, subreddit, subreddit_fnames, CSV_PATH):
-    full_df = pd.DataFrame()
-    for filename in subreddit_fnames:
-        name = os.path.splitext(os.path.basename(filename))[0]
-        np_data = np.load(filename)
-        df = pd.DataFrame(np_data, columns=["Comments"])
-        if 'val' in name: df['is_valid'] = True
-        else: df['is_valid'] = False
-        full_df = full_df.append(df)
+    def create_csv(self, subreddit, subreddit_fnames, CSV_PATH):
+        full_df = pd.DataFrame()
+        for filename in subreddit_fnames:
+            name = os.path.splitext(os.path.basename(filename))[0]
+            np_data = np.load(filename)
+            df = pd.DataFrame(np_data, columns=["Comments"])
+            if 'val' in name:
+                df['is_valid'] = True
+            else:
+                df['is_valid'] = False
+            full_df = full_df.append(df)
 
-    full_df.reset_index(inplace=True,drop=True)
-    full_df.to_csv(CSV_PATH/str(subreddit+".csv"))
-    return
-  
-  def create_save_databunch_pkl(self,subreddit, batch_size, CSV_PATH, PKL_PATH):
-    data = (TextList.from_csv(CSV_PATH, str(subreddit+".csv"), cols='Comments')
+        print("Before removing NaNs - ", full_df.shape)
+        full_df = full_df[full_df['Comments'] != '']
+        full_df.dropna(inplace=True)
+        print("After removing NaNs - ", full_df.shape)
+        full_df.reset_index(inplace=True, drop=True)
+        full_df.to_csv(CSV_PATH/str(subreddit+".csv"))
+        return
+
+    def create_save_databunch_pkl(self, subreddit, batch_size, CSV_PATH, PKL_PATH):
+        data = (fastai_text.TextList.from_csv(CSV_PATH, str(subreddit+".csv"), cols='Comments')
                 .split_from_df(col='is_valid')
                 .label_for_lm()
                 .databunch(bs=batch_size))
-    temp = data.path
-    data.path = PKL_PATH
-    data.save(str(subreddit+".pkl"))
-    data.path = temp
-    return
+        temp = data.path
+        data.path = PKL_PATH
+        data.save(str(subreddit+".pkl"))
+        data.path = temp
+        return
 
 
 def main():
 
-  BASE_PATH = Path('../dataset/100k')
-  ZIP_PATH = BASE_PATH/'zip-data'
-  UNZIP_PATH = BASE_PATH/'unzip-data'
-  CSV_PATH = BASE_PATH/'csv-data'
-  PKL_PATH = BASE_PATH/'pkl-data'
+    BASE_PATH = fastai_text.Path('../dataset/')
+    ZIP_PATH = BASE_PATH/'zip-data'
+    # UNZIP_PATH = BASE_PATH/'unzip-data'
+    UNZIP_PATH = BASE_PATH
+    CSV_PATH = BASE_PATH/'csv-data'
+    PKL_PATH = BASE_PATH/'pkl-data'
 
-  BATCH_SIZE = 32
+    BATCH_SIZE = 32
 
-  subreddits = ['news_100k'] #'news_100k'
+    subreddits = [
+        "aww_100000",
+        "conservative_100000",
+        "democrat_100000",
+        "elizabethwarren_100000",
+        "impeach_trump_100000",
+        "libertarian_100000",
+        "news_100000",
+        "ourpresident_100000",
+        "politics_100000",
+        "sandersforpresident_100000",
+        "soccer_100000",
+        "the_donald_100000",
+        "the_mueller_100000",
+        "yangforpresidenthq_100000",
+    ]
 
-  data_pre = DataProcessing()
+    data_pre = DataProcessing()
 
-  for subreddit in subreddits:
-    print ("*"*50, subreddit, "Pre-processing", "*"*50)
+    for subreddit in subreddits:
+        print("*"*50, subreddit, "Pre-processing", "*"*50)
 
-    print ("Step 1/4 - Unzipping {}...".format(subreddit))
-    # %time data_pre.unzip_data(ZIP_PATH/str(subreddit+".zip"), UNZIP_PATH)
+        print("Step 1/4 - Unzipping {}...".format(subreddit))
+        # %time data_pre.unzip_data(ZIP_PATH/str(subreddit+".zip"), UNZIP_PATH)
 
-    print ("Step 2/4 - Extracting all filenames from unzipped folder...")
-    # subreddit_fnames = data_pre.get_filenames(UNZIP_PATH/subreddit, '*.npy')
-    # for name in subreddit_fnames: print (name)
+        print("Step 2/4 - Extracting all filenames from unzipped folder...")
+        subreddit_fnames = data_pre.get_filenames(
+            UNZIP_PATH, subreddit+'*.npy')
+        for name in subreddit_fnames:
+            print(name)
 
-    print ("Step 3/4 - Combining files to create data csv...")
-    # %time data_pre.create_csv(subreddit, subreddit_fnames, CSV_PATH)
+        print("Step 3/4 - Combining files to create data csv...")
+        data_pre.create_csv(subreddit, subreddit_fnames, CSV_PATH)
 
-    print ("Step 4/4 - Creating and Saving databunch from csv...")
-    # %time data_pre.create_save_databunch_pkl(subreddit, BATCH_SIZE, CSV_PATH, PKL_PATH)
+        print("Step 4/4 - Creating and Saving databunch from csv...")
+        data_pre.create_save_databunch_pkl(
+            subreddit, BATCH_SIZE, CSV_PATH, PKL_PATH)
+
 
 if __name__ == "__main__":
     main()
--- code/models/ulmfit.py
+++ code/models/ulmfit.py
@@ -1,58 +1,80 @@
 from fastai.text import *
 
+
 class SubredditULMFit(object):
 
-  def load_databunch(self, subreddit, PKL_PATH, BATCH_SIZE):
-    data = load_data(PKL_PATH, str(subreddit + ".pkl"), bs=BATCH_SIZE)
-    return data
+    def load_databunch(self, subreddit, PKL_PATH, BATCH_SIZE):
+        data = load_data(PKL_PATH, str(subreddit + ".pkl"), bs=BATCH_SIZE)
+        return data
+
 
 def main():
 
-  BASE_PATH = Path('drive/My Drive/Google Colab/wstm-project/100k/')
-  ZIP_PATH = BASE_PATH/'zip-data'
-  UNZIP_PATH = BASE_PATH/'unzip-data'
-  CSV_PATH = BASE_PATH/'csv-data'
-  PKL_PATH = BASE_PATH/'pkl-data'
-  IMG_PATH = BASE_PATH/'img-data'
-  BATCH_SIZE = 32
+    # Path('drive/My Drive/Google Colab/wstm-project/100k/')
+    BASE_PATH = Path('../dataset')
 
-  subreddits = ['news_100k'] #'news_100k'
-    
-  reddit_ulmfit = SubredditULMFit()
-  finding_lr = True
+    RESULTS_PATH = Path('../results/ulmfit')
+    # ZIP_PATH = BASE_PATH/'zip-data'
+    UNZIP_PATH = BASE_PATH  # BASE_PATH/'unzip-data'
+    CSV_PATH = BASE_PATH/'csv-data'
+    PKL_PATH = BASE_PATH/'pkl-data'
+    IMG_PATH = RESULTS_PATH/'img-data'
 
-  for subreddit in subreddits:
-    print ("*"*50, subreddit, "Training", "*"*50)
+    BATCH_SIZE = 64
 
-    print ("Step 1/7 - Loading DataBunch {}...".format(subreddit))
-    data = reddit_ulmfit.load_databunch(subreddit, PKL_PATH, BATCH_SIZE)
+    subreddits = [
+        "aww_100000",
+        "conservative_100000",
+        "democrat_100000",
+        "elizabethwarren_100000",
+        "impeach_trump_100000",
+        "libertarian_100000",
+        "news_100000",
+        "ourpresident_100000",
+        "politics_100000",
+        "sandersforpresident_100000",
+        "soccer_100000",
+        "the_donald_100000",
+        "the_mueller_100000",
+        "yangforpresidenthq_100000",
+    ]
+    reddit_ulmfit = SubredditULMFit()
+    finding_lr = False
 
-    print ("Step 2/7 - Loading pre-trained wikitext language model...")
-    learn = language_model_learner(data, AWD_LSTM, drop_mult=0.3)
+    lr = 3e-2
 
-    print ("Step 3/7 - Observe best learning rate...")
-    learn.lr_find()
-    
-    print ("Step 4/7 - Saving figure for learning rate...")
-    fig = learn.recorder.plot(return_fig=True)
-    fig.savefig(IMG_PATH/str(subreddit + ".png"))
+    for subreddit in subreddits:
+        print("*"*50, subreddit, "Training", "*"*50)
 
-    if (finding_lr):
-      print ("Exiting at Step 4...")
-      break
+        print("Step 1/7 - Loading DataBunch {}...".format(subreddit))
+        data = reddit_ulmfit.load_databunch(subreddit, PKL_PATH, BATCH_SIZE)
 
-    print ("Step 4/7 - Fit data to only last few layers")
-    learn.fit_one_cycle(1, lr, moms=(0.8,0.7))
+        print("Step 2/7 - Loading pre-trained wikitext language model...")
+        learn = language_model_learner(data, AWD_LSTM, drop_mult=0.3)
 
-    print ("Step 5/7 - Saving stage 1 model")
-    learn.save(str(subreddit + '_stage1'))
+        print("Step 3/7 - Observe best learning rate...")
+        learn.lr_find()
 
-    print ("Step 6/7 - Unfreeze the network and fit data to all layers")
-    learn.unfreeze()
-    learn.fit_one_cycle(1, 1e-3, moms=(0.8,0.7))
+        print("Step 4/7 - Saving figure for learning rate...")
+        fig = learn.recorder.plot(return_fig=True)
+        fig.savefig(IMG_PATH/str(subreddit + ".png"))
 
-    print ("Step 7/7 - Saving stage 2 model")
-    learn.save(str(subreddit + '_stage2'))
+        if (finding_lr):
+            print("Exiting at Step 4...")
+            continue
+
+        print("Step 4/7 - Fit data to only last few layers")
+        learn.fit_one_cycle(1, lr, moms=(0.8, 0.7))
+
+        print("Step 5/7 - Saving stage 1 model")
+        learn.save(str(subreddit + '_stage1'))
+
+        print("Step 6/7 - Unfreeze the network and fit data to all layers")
+        learn.unfreeze()
+        learn.fit_one_cycle(15, 1e-3, moms=(0.8, 0.7))
+
+        print("Step 7/7 - Saving stage 2 model")
+        learn.save(str(subreddit + '_stage2'))
 
 
 if __name__ == "__main__":
